897d9fb299ca701dc30d8867f594ba005b83e0cd
chiko99989 <chiko99989@googlemail.com>
2018-04-27 00:46:13 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -136,8 +136,8 @@ public:
         printf("%s\n", genesis.GetHash().ToString().c_str());
         printf("%s\n", genesis.BuildMerkleTree().ToString().c_str());
 
-        assert(hashGenesisBlock == uint256("0x65cb33897e50dbf348b264705789f44f6696b9d96b7f448d6be30cfe6ae08872"));
-        assert(genesis.hashMerkleRoot == uint256("0xab2caaf670ce9e626578c48e40c24a296396f189823d56fe762803ca9b9d2676"));
+        assert(hashGenesisBlock == uint256("0xf86c0facbd4f8c7b7387ced2faff3c28e2c707058364c9fcd57a34c558a2a3aa"));
+        assert(genesis.hashMerkleRoot == uint256("0xc0a27673e326d60044bb232de9406af166326759ef835f73acdd72e4f1f44fd8"));
 
         vSeeds.push_back(CDNSSeedData("85.214.211.186", "85.214.211.186")); // Non-standard DNS request
 
